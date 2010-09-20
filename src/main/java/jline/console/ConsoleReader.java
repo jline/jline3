jline/jline1

Restore original behaviour to put cursor on beginning of match in CTRL-R search.

src/main/java/jline/ConsoleReader.java
@@ -1781,7 +1781,7 @@ public class ConsoleReader implements ConsoleOperations {
         int i = match.indexOf(searchTerm);
         printString("\r(reverse-i-search) `" + searchTerm + "': " + match + "\u001b[K");
         // FIXME: our ANSI using back() does not work here
-        printCharacters(BACKSPACE, match.length());
+        printCharacters(BACKSPACE, match.length() - i);
         flushConsole();
     }
 

jline/jline3

src/main/java/jline/console/ConsoleReader.java
@@ -1734,7 +1734,7 @@ public class ConsoleReader
         int i = match.indexOf(searchTerm);
         print("\r(reverse-i-search) `" + searchTerm + "': " + match + "\u001b[K");
         // FIXME: our ANSI using back() does not work here
-        print(BACKSPACE, match.length());
+        print(BACKSPACE, match.length() - i);
         flush();
     }
 