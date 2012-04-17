6972eafad2780e8e177d83ec4560127078b63ce0
Jason Dillon <jason@planet57.com>
2012-04-16 20:16:03 -0700

    Reformat headers

--- src/main/java/jline/internal/NonBlockingInputStream.java
+++ src/main/java/jline/internal/NonBlockingInputStream.java
@@ -1,8 +1,10 @@
 /*
- * Copyright (c) 2012, Scott C. Gray. All rights reserved.
+ * Copyright (c) 2002-2012, the original author or authors.
  *
  * This software is distributable under the BSD license. See the terms of the
  * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
  */
 package jline.internal;
 
--- src/test/java/jline/console/ConsoleReaderTest.java
+++ src/test/java/jline/console/ConsoleReaderTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2002-2012, the original author or authors.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
 package jline.console;
 
 import java.io.ByteArrayInputStream;
