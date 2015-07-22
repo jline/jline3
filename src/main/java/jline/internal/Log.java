52082c039339e547e868e342a4a7b29a23759494
Guillaume Nodet <gnodet@apache.org>
2015-07-22 14:21:51 +0200

    Support JUL as a logging mechanism Fixes #93

--- src/main/java/jline/internal/Log.java
+++ src/main/java/jline/internal/Log.java
@@ -8,7 +8,10 @@
  */
 package jline.internal;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import static jline.internal.Preconditions.checkNotNull;
 
@@ -16,6 +19,7 @@ import static jline.internal.Preconditions.checkNotNull;
  * Internal logger.
  *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @author <a href="mailto:gnodet@gmail.com">Guillaume Nodet</a>
  * @since 2.0
  */
 public final class Log
@@ -31,11 +35,13 @@ public final class Log
         ERROR
     }
 
-    public static final boolean TRACE = Boolean.getBoolean(Log.class.getName() + ".trace");
+    public static final boolean TRACE = Configuration.getBoolean(Log.class.getName() + ".trace");
 
-    public static final boolean DEBUG = TRACE || Boolean.getBoolean(Log.class.getName() + ".debug");
+    public static final boolean DEBUG = TRACE || Configuration.getBoolean(Log.class.getName() + ".debug");
 
     private static PrintStream output = System.err;
+
+    private static boolean useJul = Configuration.getBoolean("jline.log.jul");
 
     public static PrintStream getOutput() {
         return output;
@@ -69,6 +75,10 @@ public final class Log
 
     @TestAccessible
     static void log(final Level level, final Object... messages) {
+        if (useJul) {
+            logWithJul(level, messages);
+            return;
+        }
         //noinspection SynchronizeOnNonFinalField
         synchronized (output) {
             output.format("[%s] ", level);
@@ -86,6 +96,43 @@ public final class Log
 
             output.println();
             output.flush();
+        }
+    }
+
+    static void logWithJul(Level level, Object... messages) {
+        Logger logger = Logger.getLogger("jline");
+        Throwable cause = null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        for (int i = 0; i < messages.length; i++) {
+            // Special handling for the last message if its a throwable, render its stack on the next line
+            if (i + 1 == messages.length && messages[i] instanceof Throwable) {
+                cause = (Throwable) messages[i];
+            }
+            else {
+                render(ps, messages[i]);
+            }
+        }
+        ps.close();
+        LogRecord r = new LogRecord(toJulLevel(level), baos.toString());
+        r.setThrown(cause);
+        logger.log(r);
+    }
+
+    private static java.util.logging.Level toJulLevel(Level level) {
+        switch (level) {
+            case TRACE:
+                return java.util.logging.Level.FINEST;
+            case DEBUG:
+                return java.util.logging.Level.FINE;
+            case INFO:
+                return java.util.logging.Level.INFO;
+            case WARN:
+                return java.util.logging.Level.WARNING;
+            case ERROR:
+                return java.util.logging.Level.SEVERE;
+            default:
+                throw new IllegalArgumentException();
         }
     }
 
