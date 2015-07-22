8e4316d5d670c63b747e5eb8eef4af48af8e0d21
Guillaume Nodet <gnodet@apache.org>
2015-07-22 13:31:25 +0200

    Forgot to add new files in commit 89c54a94b8f98506be82c342d8652d454e5361cc

---
+++ src/main/java/jline/internal/Ansi.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2002-2012, the original author or authors.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package jline.internal;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.fusesource.jansi.AnsiOutputStream;
+
+/**
+ * Ansi support.
+ *
+ * @author <a href="mailto:gnodet@gmail.com">Guillaume Nodet</a>
+ * @since 2.13
+ */
+public class Ansi {
+
+    public static String stripAnsi(String str) {
+        if (str == null) return "";
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            AnsiOutputStream aos = new AnsiOutputStream(baos);
+            aos.write(str.getBytes());
+            aos.close();
+            return baos.toString();
+        } catch (IOException e) {
+            return str;
+        }
+    }
+
+}
