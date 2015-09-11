3795b9e55cc8cfe2d5248ebf6789b1ce4572b5ec
Guillaume Nodet <gnodet@apache.org>
2015-09-11 08:08:54 +0200

    Move test resources to org/jline

--- src/test/java/org/jline/reader/ConsoleKeysTest.java
+++ src/test/java/org/jline/reader/ConsoleKeysTest.java
@@ -9,26 +9,26 @@ public class ConsoleKeysTest extends ReaderTestSupport {
 
     @Test
     public void testInputBadConfig() throws Exception {
-        ConsoleKeys keys = new ConsoleKeys("Bash", getClass().getResource("/jline/internal/config-bad"));
+        ConsoleKeys keys = new ConsoleKeys("Bash", getClass().getResource("/org/jline/internal/config-bad"));
         assertEquals(new Macro("\u001bb\"\u001bf\""),
                 keys.getKeys().getBound(ctrl('X') + "q"));
     }
 
     @Test
     public void testInput() throws Exception {
-        ConsoleKeys keys = new ConsoleKeys("JLine", getClass().getResource("/jline/internal/config1"));
+        ConsoleKeys keys = new ConsoleKeys("JLine", getClass().getResource("/org/jline/internal/config1"));
         assertEquals(Operation.UNIVERSAL_ARGUMENT, keys.getKeys().getBound("" + ctrl('U')));
         assertEquals(new Macro("Function Key \u2671"), keys.getKeys().getBound("\u001b[11~"));
         assertEquals(null, keys.getKeys().getBound(ctrl('X') + "q"));
 
-        keys = new ConsoleKeys("bash", getClass().getResource("/jline/internal/config1"));
+        keys = new ConsoleKeys("bash", getClass().getResource("/org/jline/internal/config1"));
         assertEquals(new Macro("\u001bb\"\u001bf\""),
                 keys.getKeys().getBound(ctrl('X') + "q"));
     }
 
     @Test
     public void testInput2() throws Exception {
-        ConsoleKeys keys = new ConsoleKeys("Bash", getClass().getResource("/jline/internal/config2"));
+        ConsoleKeys keys = new ConsoleKeys("Bash", getClass().getResource("/org/jline/internal/config2"));
         assertNotNull(keys.getKeys().getBound("\u001b" + ctrl('V')));
     }
 
