5051a8313e248417c9696dbe67ac70e84f5bf38c
Guillaume Nodet <gnodet@apache.org>
2016-09-06 10:52:46 +0200

    Fix display when using mouse scroll in less

--- src/main/java/org/jline/builtins/Less.java
+++ src/main/java/org/jline/builtins/Less.java
@@ -577,7 +577,7 @@ public class Less {
         if (buffer.length() > 0) {
             msg.append(" ").append(buffer);
         } else if (bindingReader.getCurrentBuffer().length() > 0
-                && terminal.reader().peek(1) != NonBlockingReader.READ_EXPIRED) {
+                && terminal.reader().peek(1) == NonBlockingReader.READ_EXPIRED) {
             msg.append(" ").append(printable(bindingReader.getCurrentBuffer()));
         } else if (message != null) {
             msg.style(AttributedStyle.INVERSE);
