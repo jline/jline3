0e30d9fc7a881a7349579ed2cd4b44a32a3c4e84
Guillaume Nodet <gnodet@apache.org>
2016-12-13 09:05:04 +0100

    JDK 9 compatibility

--- src/main/java/org/jline/terminal/impl/jna/JnaNativePty.java
+++ src/main/java/org/jline/terminal/impl/jna/JnaNativePty.java
@@ -120,7 +120,7 @@ public abstract class JnaNativePty implements Pty {
             Constructor<FileDescriptor> cns = FileDescriptor.class.getDeclaredConstructor(int.class);
             cns.setAccessible(true);
             return cns.newInstance(fd);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             throw new RuntimeException("Unable to create FileDescriptor", e);
         }
     }
---
+++ src/test/java/org/jline/terminal/impl/jna/JnaNativePtyTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2002-2016, the original author or authors.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package org.jline.terminal.impl.jna;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class JnaNativePtyTest {
+
+    @Test
+    public void testDescriptor() {
+        assertNotNull(JnaNativePty.newDescriptor(4));
+    }
+}
