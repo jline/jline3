ee0a721fa29fd2549c7e035eaa0297dd348931f9
Guillaume Nodet <gnodet@apache.org>
2016-12-13 08:36:01 +0100

    Remove jline version from the windows demo script

--- demo/jline-gogo.bat
+++ demo/jline-gogo.bat
@@ -4,35 +4,45 @@ set DIRNAME=%~dp0%
 set ROOTDIR=%DIRNAME%\..
 set TARGETDIR=%ROOTDIR%\target
 
-set JLINE_VERSION=3.1.0-SNAPSHOT
 set JANSI_VERSION=1.14
 set JNA_VERSION=4.2.2
 set GOGO_RUNTIME_VERSION=1.0.0
 set GOGO_JLINE_VERSION=1.0.0
 
 rem initialization
-if not exist %TARGETDIR%\jline-%JLINE_VERSION%.jar (
-    echo Build jline with maven before running the demo
-    goto END
-)
 if not exist %TARGETDIR%\lib (
   mkdir %TARGETDIR%\lib
 )
 
 rem JLINE
-set cp=%TARGETDIR%\jline-%JLINE_VERSION%.jar
+set cp=""
+pushd %TARGETDIR%
+for %%G in (*.jar) do call:APPEND_TO_CLASSPATH %%G
+popd
+if not "%cp" == "" goto CLASSPATH_END
+    echo Build jline with maven before running the demo
+    goto END
+: APPEND_TO_CLASSPATH
+set filename=%~1
+set suffix=%filename:~-4%
+if %suffix% equ .jar set cp=%cp%;%TARGETDIR%\%filename%
+goto :EOF
+
+:CLASSPATH_END
 
 rem JANSI
 if not exist %TARGETDIR%\lib\jansi-%JANSI_VERSION%.jar (
   echo "Downloading Jansi..."
   %DIRNAME%\wget.exe -O %TARGETDIR%\lib\jansi-%JANSI_VERSION%.jar http://repo1.maven.org/maven2/org/fusesource/jansi/jansi/%JANSI_VERSION%/jansi-%JANSI_VERSION%.jar
 )
+set cp=%cp%;%TARGETDIR%\lib\jansi-%JANSI_VERSION%.jar
 
 rem JNA
 if not exist %TARGETDIR%\lib\jna-%JNA_VERSION%.jar (
   echo "Downloading JNA..."
   %DIRNAME%\wget.exe -O %TARGETDIR%\lib\jna-%JNA_VERSION%.jar http://repo1.maven.org/maven2/net/java/dev/jna/jna/%JNA_VERSION%/jna-%JNA_VERSION%.jar
 )
+set cp=%cp%;%TARGETDIR%\lib\jna-%JNA_VERSION%.jar
 
 rem Gogo Runtime
 if not exist %TARGETDIR%\lib\org.apache.felix.gogo.runtime-%GOGO_RUNTIME_VERSION%.jar (
