cb941af977c946e02feb801a3f66f5efbd322313
Guillaume Nodet <gnodet@apache.org>
2017-04-06 18:00:47 +0200

    Fix wrong test assertion

--- builtins/src/test/java/org/jline/builtins/TmuxTest.java
+++ builtins/src/test/java/org/jline/builtins/TmuxTest.java
@@ -25,7 +25,7 @@ public class TmuxTest {
     public void testLayoutResize() {
         Tmux.Layout l = Tmux.Layout.parse("b7c7,148x44,0,0[148x26,0,0{69x26,0,0,0,78x26,70,0,3},148x17,0,27{74x17,0,27,1,36x17,75,27,4,36x17,112,27,5}]");
         l.resize(140, 44);
-        assertEquals("f080,140x44,0,0[140x26,0,0{65x26,0,0,0,74x26,70,0,0},140x17,0,27{71x17,0,27,0,33x17,75,27,0,34x17,112,27,0}]", l.dump());
+        assertEquals("ebac,140x44,0,0[140x26,0,0{65x26,0,0,0,74x26,66,0,0},140x17,0,27{71x17,0,27,0,33x17,72,27,0,34x17,106,27,0}]", l.dump());
         System.out.println(l.dump());
     }
 }
