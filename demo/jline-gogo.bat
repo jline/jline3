e2f7d2eb16d039c23d1f9b76d4997b71e3c1cb40
Guillaume Nodet <gnodet@apache.org>
2017-04-27 17:49:20 +0200

    Simplify the demo

--- demo/jline-gogo.bat
+++ demo/jline-gogo.bat
@@ -2,81 +2,51 @@
 
 set DIRNAME=%~dp0%
 set ROOTDIR=%DIRNAME%\..
-set TARGETDIR=%ROOTDIR%\target
-
-set JANSI_VERSION=1.14
-set JNA_VERSION=4.2.2
-set GOGO_RUNTIME_VERSION=1.0.2
-set GOGO_JLINE_VERSION=1.0.2
+set TARGETDIR=%DIRNAME%target
 
 rem initialization
 if not exist %TARGETDIR%\lib (
-  mkdir %TARGETDIR%\lib
-)
-
-rem JLINE
-set cp=""
-pushd %TARGETDIR%
-for %%G in (*.jar) do call:APPEND_TO_CLASSPATH %%G
-popd
-if not "%cp" == "" goto CLASSPATH_END
     echo Build jline with maven before running the demo
     goto END
+)
+
+goto :SETUP_CLASSPATH
+
 : APPEND_TO_CLASSPATH
 set filename=%~1
-set suffix=%filename:~-4%
-if %suffix% equ .jar set cp=%cp%;%TARGETDIR%\%filename%
+set cp=%cp%;%TARGETDIR%\lib\%filename%
 goto :EOF
 
-:CLASSPATH_END
-
-rem JANSI
-if not exist %TARGETDIR%\lib\jansi-%JANSI_VERSION%.jar (
-  echo "Downloading Jansi..."
-  %DIRNAME%\wget.exe -O %TARGETDIR%\lib\jansi-%JANSI_VERSION%.jar http://repo1.maven.org/maven2/org/fusesource/jansi/jansi/%JANSI_VERSION%/jansi-%JANSI_VERSION%.jar
-)
-set cp=%cp%;%TARGETDIR%\lib\jansi-%JANSI_VERSION%.jar
-
-rem JNA
-if not exist %TARGETDIR%\lib\jna-%JNA_VERSION%.jar (
-  echo "Downloading JNA..."
-  %DIRNAME%\wget.exe -O %TARGETDIR%\lib\jna-%JNA_VERSION%.jar http://repo1.maven.org/maven2/net/java/dev/jna/jna/%JNA_VERSION%/jna-%JNA_VERSION%.jar
-)
-set cp=%cp%;%TARGETDIR%\lib\jna-%JNA_VERSION%.jar
-
+:SETUP_CLASSPATH
+set cp=%TARGETDIR%\classes
+rem JLINE
+pushd %TARGETDIR%\lib
+for %%G in (jline-*.jar) do call:APPEND_TO_CLASSPATH %%G
 rem Gogo Runtime
-if not exist %TARGETDIR%\lib\org.apache.felix.gogo.runtime-%GOGO_RUNTIME_VERSION%.jar (
-  echo "Downloading Gogo Runtime..."
-  %DIRNAME%\wget.exe -O %TARGETDIR%\lib\org.apache.felix.gogo.runtime-%GOGO_RUNTIME_VERSION%.jar http://repo1.maven.org/maven2/org/apache/felix/org.apache.felix.gogo.runtime/%GOGO_RUNTIME_VERSION%/org.apache.felix.gogo.runtime-%GOGO_RUNTIME_VERSION%.jar
-)
-set cp=%cp%;%TARGETDIR%\lib\org.apache.felix.gogo.runtime-%GOGO_RUNTIME_VERSION%.jar
-
+for %%G in (org.apache.felix.gogo.runtime-*.jar) do call:APPEND_TO_CLASSPATH %%G
 rem Gogo JLine
-if not exist %TARGETDIR%\lib\org.apache.felix.gogo.jline-%GOGO_JLINE_VERSION%.jar (
-  echo "Downloading Gogo JLine..."
-  %DIRNAME%\wget.exe -O %TARGETDIR%\lib\org.apache.felix.gogo.jline-%GOGO_JLINE_VERSION%.jar http://repo1.maven.org/maven2/org/apache/felix/org.apache.felix.gogo.jline/%GOGO_JLINE_VERSION%/org.apache.felix.gogo.jline-%GOGO_JLINE_VERSION%.jar
-)
-set cp=%cp%;%TARGETDIR%\lib\org.apache.felix.gogo.jline-%GOGO_JLINE_VERSION%.jar
-
+for %%G in (org.apache.felix.gogo.jline-*.jar) do call:APPEND_TO_CLASSPATH %%G
 
 set "opts=%JLINE_OPTS%"
+set "logconf=%DIRNAME%etc\logging.properties"
 :RUN_LOOP
     if "%1" == "jansi" goto :EXECUTE_JANSI
     if "%1" == "jna" goto :EXECUTE_JNA
     if "%1" == "debug" goto :EXECUTE_DEBUG
     if "%1" == "debugs" goto :EXECUTE_DEBUGS
+    if "%1" == "verbose" goto :EXECUTE_VERBOSE
     if "%1" == "" goto :EXECUTE
     set "opts=%opts% %~1"
     shift
     goto :RUN_LOOP
 
 :EXECUTE_JANSI
-    set cp=%cp%;%TARGETDIR%/lib/jansi-%JANSI_VERSION%.jar
+    for %%G in (jansi-*.jar) do call:APPEND_TO_CLASSPATH %%G
     shift
     goto :RUN_LOOP
 
 :EXECUTE_JNA
-    set cp=%cp%;%TARGETDIR%/lib/jna-%JNA_VERSION%.jar
+    for %%G in (jna-*.jar) do call:APPEND_TO_CLASSPATH %%G
     shift
     goto :RUN_LOOP
 
@@ -90,10 +60,17 @@ set "opts=%JLINE_OPTS%"
     shift
     goto :RUN_LOOP
 
+:EXECUTE_VERBOSE
+    set "logconf=%DIRNAME%etc\logging-verbose.properties"
+    shift
+    goto :RUN_LOOP
+
 :EXECUTE
+popd
+
 rem Launch gogo shell
-echo "Classpath: %cp%"
-echo "Launching Gogo JLine..."
-java -cp %cp% %opts% org.apache.felix.gogo.jline.Main
+echo Launching Gogo JLine...
+echo Classpath: %cp%
+java -cp %cp% %opts% -Dgosh.home=%DIRNAME% -Djava.util.logging.config.file=%logconf% org.apache.felix.gogo.jline.Main
 
 :END
