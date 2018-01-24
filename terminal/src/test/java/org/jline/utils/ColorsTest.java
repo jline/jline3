64629eb526181f71ac55dc78051d0732ee014ef3
Guillaume Nodet <gnodet@gmail.com>
2018-01-24 12:16:11 +0100

    Improve color rounding to 16 colors, fixes #217

---
+++ terminal/src/test/java/org/jline/utils/ColorsTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2002-2018, the original author or authors.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package org.jline.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ColorsTest {
+
+    @Test
+    public void testRounding() {
+        assertEquals(2, Colors.roundColor(71, 16, "cam02"));
+        assertEquals(2, Colors.roundColor(71, 16, "camlab(1,2)"));
+
+        assertEquals(8, Colors.roundColor(71, 16, "rgb"));
+        assertEquals(8, Colors.roundColor(71, 16, "rgb(2,4,3)"));
+        assertEquals(2, Colors.roundColor(71, 16, "cie76"));
+        assertEquals(2, Colors.roundColor(71, 16, "cie94"));
+        assertEquals(2, Colors.roundColor(71, 16, "cie00"));
+    }
+
+}
