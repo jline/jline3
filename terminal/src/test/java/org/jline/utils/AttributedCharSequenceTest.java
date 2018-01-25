2973cec7a4352df6aeaea30b9736cfc6a1bfdc83
Guillaume Nodet <gnodet@gmail.com>
2018-01-25 20:28:29 +0100

    Make sure the bold attributed is outputed after the color in ansi sequences, #218, #219

--- terminal/src/test/java/org/jline/utils/AttributedCharSequenceTest.java
+++ terminal/src/test/java/org/jline/utils/AttributedCharSequenceTest.java
@@ -8,9 +8,14 @@
  */
 package org.jline.utils;
 
+import org.jline.terminal.impl.ExternalTerminal;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,7 +30,20 @@ public class AttributedCharSequenceTest {
         //sb.append(NOR);
         AttributedString as = sb.toAttributedString();
 
-        assertEquals("\33[1;36mthe buffer\33[0m", as.toAnsi(null));
+        assertEquals("\33[36;1mthe buffer\33[0m", as.toAnsi(null));
+    }
+
+    @Test
+    public void testBold() throws IOException {
+        ExternalTerminal terminal = new ExternalTerminal(
+                "my term",
+                "windows",
+                new ByteArrayInputStream(new byte[0]),
+                new ByteArrayOutputStream(),
+                StandardCharsets.UTF_8);
+
+        assertEquals("\33[32;1mtest\33[0m", AttributedString.fromAnsi("\33[32m\33[1mtest\33[0m").toAnsi(terminal));
+        assertEquals("\33[32;1mtest\33[0m", AttributedString.fromAnsi("\33[1m\33[32mtest\33[0m").toAnsi(terminal));
     }
 
 }
