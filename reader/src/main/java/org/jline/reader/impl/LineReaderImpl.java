jline/jline3

commit feb769018aab222614e4576aa63cc746b45224cc

Autosuggestion choices are not refreshed after tab, fixes #545

reader/src/main/java/org/jline/reader/impl/LineReaderImpl.java
@@ -4010,8 +4010,7 @@ public class LineReaderImpl implements LineReader, Flushable
                 sb.styled(AttributedStyle::faint, tailTip);
                 full.append(sb.toAttributedString());
             } else if (autosuggestion == SuggestionType.COMPLETER) {
-                if (buf.length() > 0 && buf.length() == buf.cursor()
-                    && (!lastBinding.equals("\t") || buf.prevChar() == ' ' || buf.prevChar() == '=')) {
+                if (buf.length() > 0 && buf.length() == buf.cursor()) {
                     clearChoices();
                     listChoices(true);
                 } else if (!lastBinding.equals("\t")) {

commit 25824ac0f450660ac847edd40a2bfa8f14465115

Autosuggestion choices not refreshed after tab #545, fixed regression caused by commit https://github.com/jline/jline3/commit/feb769018aab222614e4576aa63cc746b45224cc

reader/src/main/java/org/jline/reader/impl/LineReaderImpl.java
@@ -4006,7 +4006,9 @@ public class LineReaderImpl implements LineReader, Flushable
                 sb.styled(AttributedStyle::faint, tailTip);
                 full.append(sb.toAttributedString());
             } else if (autosuggestion == SuggestionType.COMPLETER) {
-                if (buf.length() > 0 && buf.length() == buf.cursor()) {
+                if (buf.length() > 0 && buf.length() == buf.cursor()
+                        && (!lastBinding.equals("\t") || buf.prevChar() == ' ' || buf.prevChar() == '='
+                          || buf.prevChar() == '/' || buf.prevChar() == '.')) {
                     clearChoices();
                     listChoices(true);
                 } else if (!lastBinding.equals("\t")) {