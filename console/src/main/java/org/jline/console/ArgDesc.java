049d24a314ab0f832868389b58f9023d9e7fe4aa
mattirn <matti.rintanikkola@gmail.com>
2021-04-27 17:34:39 +0200

    ArgDesc: add validity check for argument name, #665

--- console/src/main/java/org/jline/console/ArgDesc.java
+++ console/src/main/java/org/jline/console/ArgDesc.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2020, the original author or authors.
+ * Copyright (c) 2002-2021, the original author or authors.
  *
  * This software is distributable under the BSD license. See the terms of the
  * BSD license in the documentation provided with this software.
@@ -22,6 +22,9 @@ public class ArgDesc {
     }
 
     public ArgDesc(String name, List<AttributedString> description) {
+        if (name.contains("\t") || name.contains(" ")) {
+            throw new IllegalArgumentException("Bad argument name: " + name);
+        }
         this.name = name;
         this.description = new ArrayList<>(description);
     }
