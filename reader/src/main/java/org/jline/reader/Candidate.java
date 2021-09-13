31c96a7c40551cd30f37b24c060a1384b8258e4b
snuyanzin <snuyanzin@gmail.com>
2021-09-14 00:51:04 +0200

    [JLINE-699] Make candidates for completion unique

--- reader/src/main/java/org/jline/reader/Candidate.java
+++ reader/src/main/java/org/jline/reader/Candidate.java
@@ -139,6 +139,19 @@ public class Candidate implements Comparable<Candidate> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Candidate candidate = (Candidate) o;
+        return Objects.equals(value, candidate.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
     public String toString() {
         return "Candidate{" + value + "}";
     }
