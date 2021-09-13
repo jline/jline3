74c97a23a3baceaf9efff598516d3936470009cb
snuyanzin <snuyanzin@gmail.com>
2021-09-14 00:51:04 +0200

    [JLINE-699] Make candidates for completion unique

--- reader/src/test/java/org/jline/reader/impl/CompletionWithCustomMatcherTest.java
+++ reader/src/test/java/org/jline/reader/impl/CompletionWithCustomMatcherTest.java
@@ -12,7 +12,10 @@ import org.jline.reader.*;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class CompletionWithCustomMatcherTest extends ReaderTestSupport {
@@ -30,6 +33,8 @@ public class CompletionWithCustomMatcherTest extends ReaderTestSupport {
                     // add custom matcher before typo matcher
                     int pos = matchers.size() + (LineReader.Option.COMPLETE_MATCHER_TYPO.isSet(options) ? -1 : 0);
                     matchers.add(pos, simpleMatcher(candidate -> camelMatch(line.word(), 0, candidate, 0)));
+                    Candidate c = new Candidate(line.word());
+                    assertEquals("Expected only one element", 1, matches(Arrays.asList(c, c)).size());
                 }
             }
         });
