bf2f174988f6c82ed4a207f34f4881cf08590c40
AlexB <al7bashkatov@gmail.com>
2021-12-23 16:15:34 +0200

    Linereader Candidate: tests for sorting and potential integer overflow fix added

---
+++ reader/src/test/java/org/jline/reader/CandidateTest.java
@@ -0,0 +1,68 @@
+package org.jline.reader;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CandidateTest {
+
+    @Test
+    public void testCandidatesSortedByValue() {
+        List<Candidate> candidates = Arrays.asList(
+            new Candidate("cand3", "cand3", null, null, null, null, true),
+            new Candidate("cand1", "cand1", null, null, null, null, true),
+            new Candidate("cand2", "cand2", null, null, null, null, true)
+        );
+        Collections.sort(candidates);
+
+        assertEquals("cand1", candidates.get(0).value());
+        assertEquals("cand2", candidates.get(1).value());
+        assertEquals("cand3", candidates.get(2).value());
+    }
+
+    @Test
+    public void testCandidatesSortedBySortProperty() {
+        List<Candidate> candidates = Arrays.asList(
+            new Candidate("cand3", "cand3", null, null, null, null, true, 3),
+            new Candidate("cand1", "cand1", null, null, null, null, true, 1),
+            new Candidate("cand2", "cand2", null, null, null, null, true, 2)
+        );
+        Collections.sort(candidates);
+
+        assertEquals("cand1", candidates.get(0).value());
+        assertEquals("cand2", candidates.get(1).value());
+        assertEquals("cand3", candidates.get(2).value());
+    }
+
+    @Test
+    public void testCandidatesSortedByValueAndSortProperty() {
+        List<Candidate> candidates = Arrays.asList(
+            new Candidate("cand3", "cand3", null, null, null, null, true, -3),
+            new Candidate("aaa", "cand1", null, null, null, null, true),
+            new Candidate("cand2", "cand2", null, null, null, null, true, 2)
+        );
+        Collections.sort(candidates);
+
+        assertEquals("cand3", candidates.get(0).value());
+        assertEquals("aaa", candidates.get(1).value());
+        assertEquals("cand2", candidates.get(2).value());
+    }
+
+    @Test
+    public void testCandidatesSortedByCornerSortProps() {
+        List<Candidate> candidates = Arrays.asList(
+            new Candidate("cand1", "cand1", null, null, null, null, true, 1),
+            new Candidate("cand2", "cand2", null, null, null, null, true, Integer.MAX_VALUE),
+            new Candidate("cand3", "cand3", null, null, null, null, true, Integer.MIN_VALUE)
+        );
+        Collections.sort(candidates);
+
+        assertEquals("cand3", candidates.get(0).value());
+        assertEquals("cand1", candidates.get(1).value());
+        assertEquals("cand2", candidates.get(2).value());
+    }
+}
