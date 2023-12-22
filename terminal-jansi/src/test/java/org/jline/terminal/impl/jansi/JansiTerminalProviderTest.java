ca98041ef08bb0b5a9c4327c08c1dc6eca917e31
Guillaume Nodet <gnodet@gmail.com>
2023-12-22 14:25:24 +0100

    g

--- terminal-jansi/src/test/java/org/jline/terminal/impl/jansi/JansiTerminalProviderTest.java
+++ terminal-jansi/src/test/java/org/jline/terminal/impl/jansi/JansiTerminalProviderTest.java
@@ -9,6 +9,7 @@
 package org.jline.terminal.impl.jansi;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
@@ -21,7 +22,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.fusesource.jansi.internal.CLibrary;
 import org.jline.terminal.Terminal;
+import org.jline.terminal.impl.AbstractPty;
 import org.jline.terminal.spi.SystemStream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -109,9 +112,9 @@ public class JansiTerminalProviderTest {
             int[] slave = new int[1];
             byte[] name = new byte[64];
             //            try {
-            //            CLibrary.openpty(master, slave, name, null, null);
-            //            new FileInputStream(AbstractPty.newDescriptor(master[0])).close();
-            //            new FileInputStream(AbstractPty.newDescriptor(slave[0])).close();
+            CLibrary.openpty(master, slave, name, null, null);
+            new FileInputStream(AbstractPty.newDescriptor(master[0])).close();
+            new FileInputStream(AbstractPty.newDescriptor(slave[0])).close();
             //            } catch (Throwable t) {
             //                t.printStackTrace();
             //                Class<?> cl = CLibrary.class;
