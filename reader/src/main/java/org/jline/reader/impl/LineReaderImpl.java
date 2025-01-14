c3ccc190b3ca6e4937fa0296d47a8a05bca9b481
hamza eraoui <hamza.eraoui@oracle.com>
2025-01-14 22:24:27 +0100

    chore: fix the astrike with cursor move

--- reader/src/main/java/org/jline/reader/impl/LineReaderImpl.java
+++ reader/src/main/java/org/jline/reader/impl/LineReaderImpl.java
@@ -293,8 +293,7 @@ public class LineReaderImpl implements LineReader, Flushable {
 
     protected String alternateIn;
     protected String alternateOut;
-
-    protected int numOfSecondaryPromptLines;
+    protected List<AttributedString> currentLines;
 
     public LineReaderImpl(Terminal terminal) throws IOException {
         this(terminal, terminal.getName(), null);
@@ -4183,7 +4182,7 @@ public class LineReaderImpl implements LineReader, Flushable {
                             sb.append(getInt(LINE_OFFSET, 0) + (line + 1));
                             break decode;
                         case '*':
-                            if (this.numOfSecondaryPromptLines == line + 1) {
+                            if (getCurrentLineNo(this.currentLines) == line) {
                                 sb.append("*");
                             } else {
                                 sb.append(" ");
@@ -4244,6 +4243,21 @@ public class LineReaderImpl implements LineReader, Flushable {
         return AttributedString.join(null, parts);
     }
 
+    private int getCurrentLineNo(List<AttributedString> lines) {
+        int currentLine = -1;
+        int cursor = buf.cursor();
+        int start = 0;
+        for (int l = 0; l < lines.size(); l++) {
+            int end = start + lines.get(l).length();
+            if (cursor >= start && cursor <= end) {
+                currentLine = l;
+                break;
+            }
+            start = end + 1;
+        }
+        return currentLine;
+    }
+
     private AttributedString fromAnsi(String str) {
         return AttributedString.fromAnsi(str, Collections.singletonList(0), alternateIn, alternateOut);
     }
@@ -4290,7 +4304,7 @@ public class LineReaderImpl implements LineReader, Flushable {
             buf.setLength(0);
         }
         int line = 0;
-        this.numOfSecondaryPromptLines = lines.size();
+        this.currentLines = lines;
         while (line < lines.size() - 1) {
             sb.append(lines.get(line)).append("\n");
             buf.append(lines.get(line)).append("\n");
