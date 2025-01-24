57030d4a05f3e55267967bfe88f006da15e6b2e9
Robert Stupp <snazy@snazy.de>
2025-01-24 23:30:34 +0100

    Replace `Path.toFile()` (#1158)
    In some use cases it's preferable to have for example the nanorc configs inside a jar. Although a lot of the code base use `java.nio.file.Path`, a couple of places still call `Path.toFile()`, which isn't supported for NIO's ZIP file system, which can be created for a `jar:` URL, so that the contents of the jar can be used as a (read only) file system.
    
    This PR replaces occurences of `Path.toFile()` and replaces the `File` operations with NIO's `Files`.

--- console/src/main/java/org/jline/console/impl/ConsoleEngineImpl.java
+++ console/src/main/java/org/jline/console/impl/ConsoleEngineImpl.java
@@ -245,7 +245,7 @@ public class ConsoleEngineImpl extends JlineCommandRegistry implements ConsoleEn
                 }
             }
             for (Path p : scripts) {
-                String name = p.toFile().getName();
+                String name = p.getFileName().toString();
                 int idx = name.lastIndexOf(".");
                 out.put(name.substring(0, idx), name.substring(idx + 1).equals(scriptExtension));
             }
@@ -395,7 +395,7 @@ public class ConsoleEngineImpl extends JlineCommandRegistry implements ConsoleEn
                         for (String e : scriptExtensions()) {
                             String file = command + "." + e;
                             Path path = Paths.get(p, file);
-                            if (path.toFile().exists()) {
+                            if (Files.exists(path)) {
                                 script = path;
                                 scriptExtension(command);
                                 found = true;
@@ -959,7 +959,7 @@ public class ConsoleEngineImpl extends JlineCommandRegistry implements ConsoleEn
                         : engine.getSerializationFormats().get(0);
                 try {
                     Path path = Paths.get(arg);
-                    if (path.toFile().exists()) {
+                    if (Files.exists(path)) {
                         if (!format.equals(SLURP_FORMAT_TEXT)) {
                             out = slurp(path, encoding, format);
                         } else {
