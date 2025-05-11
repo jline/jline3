997b8750597c014cc12bb6e7a8c858fd52432345
Guillaume Nodet <gnodet@gmail.com>
2025-05-11 23:40:00 +0200

    docs: fix javadoc redirect URL issue (#1266)
    The website javadoc link was redirecting to an invalid URL because it was using Docusaurus's <Redirect> component which is designed for internal redirects.
    
    This change:
    - Replaces the <Redirect> component with a client-side redirect using window.location.href
    - Adds a loading page with a fallback link in case the automatic redirect doesn't work
    - Ensures proper redirection to the external javadoc URL at javadoc.io
    
    Fixes jline/jline3#1115

--- website/src/pages/javadoc.tsx
+++ website/src/pages/javadoc.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import { Redirect } from '@docusaurus/router';
 
 export default function JavadocPage(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   const jlineVersion = siteConfig.customFields.jlineVersion as string;
-  
+
   // Redirect to the Maven Central Javadoc
   const javadocUrl = `https://javadoc.io/doc/org.jline/jline/${jlineVersion}/index.html`;
-  
+
+  useEffect(() => {
+    // Use window.location for a proper external redirect
+    window.location.href = javadocUrl;
+  }, [javadocUrl]);
+
   return (
     <Layout
       title="Javadoc"
       description="JLine API Documentation">
-      <Redirect to={javadocUrl} />
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <p>Redirecting to Javadoc...</p>
+        <p>
+          <a href={javadocUrl}>Click here if you are not redirected automatically</a>
+        </p>
+      </div>
     </Layout>
   );
 }
