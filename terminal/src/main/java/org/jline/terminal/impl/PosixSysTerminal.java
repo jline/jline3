d41dcb90ce4d444b514a1ca11b37dba8f2eb2e54
Guillaume Nodet <gnodet@gmail.com>
2025-05-22 10:32:54 +0200

    Fix missing close in PosixSysTerminal. (#1281)
    Fixes https://issues.apache.org/jira/browse/MNG-8733

--- terminal/src/main/java/org/jline/terminal/impl/PosixSysTerminal.java
+++ terminal/src/main/java/org/jline/terminal/impl/PosixSysTerminal.java
@@ -121,6 +121,7 @@ public class PosixSysTerminal extends AbstractPosixTerminal {
 
     @Override
     protected void doClose() throws IOException {
+        writer.close();
         ShutdownHooks.remove(closer);
         for (Map.Entry<Signal, Object> entry : nativeHandlers.entrySet()) {
             Signals.unregister(entry.getKey().name(), entry.getValue());
