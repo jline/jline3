b2e362a49d01ef49395e9c1e22d2c0a8521c2427
Guillaume Nodet <gnodet@gmail.com>
2026-04-23 21:23:03 +0100

    fix: deduplicate ReaderTestSupport between reader and builtins (#1807)
    Use a test-jar dependency from the reader module instead of maintaining
    a separate copy in builtins. Configure --patch-module for JPMS
    compatibility during test compilation and execution.

--- builtins/src/test/java/org/jline/builtins/TreeCompleterTest.java
+++ builtins/src/test/java/org/jline/builtins/TreeCompleterTest.java
@@ -10,6 +10,7 @@ package org.jline.builtins;
 
 import org.jline.builtins.Completers.TreeCompleter;
 import org.jline.reader.LineReader;
+import org.jline.reader.impl.ReaderTestSupport;
 import org.junit.jupiter.api.Test;
 
 import static org.jline.builtins.Completers.TreeCompleter.node;
